a811609cf0314bbb98bfa96c90ba3a9829ba1ebc
ConnerDuling <conner.m.duling@gmail.com>
2021-03-24 18:49:51 -0400

    renamed variables in update/movement

--- static/javascriptFiles/update.js
+++ static/javascriptFiles/update.js
@@ -1,33 +1,33 @@
 function update(){
 
-    movement;
+    movement(this);
 
 }
 
-function movement(){
+function movement(gameObject){
     
-    this.cursorKeys = this.input.keyboard.createCursorKeys();
+    var cursorKeys = gameObject.input.keyboard.createCursorKeys();
 
     var speed = 5;
     
     //Calculates the real speed if moving diagonally
     //and updates speed to the derived number for that frame.
-    if((this.cursorKeys.right.isDown || this.cursorKeys.left.isDown) &&
-    (this.cursorKeys.down.isDown || this.cursorKeys.up.isDown)){
+    if((cursorKeys.right.isDown || cursorKeys.left.isDown) &&
+    (cursorKeys.down.isDown || cursorKeys.up.isDown)){
         speed = Math.sqrt((speed * speed) / 2.0)
     }
     //Checks if user input of direction
     //keys are pressed for that frame
-    if(this.cursorKeys.right.isDown){
+    if(cursorKeys.right.isDown){
         circle.x += speed;
     }
-    if(this.cursorKeys.left.isDown){
+    if(cursorKeys.left.isDown){
         circle.x -= speed;
     }
-    if(this.cursorKeys.up.isDown){
+    if(cursorKeys.up.isDown){
         circle.y -= speed;
     }
-    if(this.cursorKeys.down.isDown){
+    if(cursorKeys.down.isDown){
         circle.y += speed;
     }
 }
