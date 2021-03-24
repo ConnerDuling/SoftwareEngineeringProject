e858e2bdb7ae64e56a48447a3d1ba75d265f5af3
ConnerDuling <conner.m.duling@gmail.com>
2021-03-23 22:24:05 -0400

    Simplified the lazy python backend

--- simpleServer.py
+++ simpleServer.py
@@ -6,9 +6,4 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def simpleServer():
 
-    if request.method == "POST":
-        
-        return render_template('index.html')
-
-    else:
-        return render_template('index.html')
+    return render_template('index.html')
