d003af00f5a33be9914ffa0817f8b0a60fddfc4a
Jacob Drew <Drewje01@pfw.edu>
2021-03-24 18:26:18 -0400

    adding wall

--- static/javascriptFiles/preload.js
+++ static/javascriptFiles/preload.js
@@ -2,4 +2,5 @@ function preload ()
     {
         this.load.image('carpet', 'static/assets/images/CarpetBackground.png');
         this.load.image('circle', 'static/assets/images/GreenCircleTransperantBackground.png');
+        this.load.image('wall', 'static/assets/images/wall_but_cool.png')
     }
