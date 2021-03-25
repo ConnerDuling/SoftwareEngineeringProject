c38636e14df233ce0beadfcc45865bccce38ec28
ConnerDuling <conner.m.duling@gmail.com>
2021-03-24 21:13:01 -0400

    Changed naming system of files

--- static/javascriptFiles/preload.js
+++ static/javascriptFiles/preload.js
@@ -1,6 +1,6 @@
 function preload ()
     {
         this.load.image('carpet', 'static/assets/images/CarpetBackground.png');
-        this.load.image('circle', 'static/assets/images/GreenCircleTransperantBackground.png');
-        this.load.image('wall', 'static/assets/images/wall_but_cool.png')
+        this.load.image('circle', 'static/assets/images/CircleGreenTransperantBackground.png');
+        this.load.image('wall', 'static/assets/images/protoWall.png')
     }
