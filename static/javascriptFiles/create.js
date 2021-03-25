612164a69eb4c52266ba9504f65a901a4b561919
ConnerDuling <conner.m.duling@gmail.com>
2021-03-24 20:19:08 -0400

    Got collision and collision detection figured out

--- static/javascriptFiles/create.js
+++ static/javascriptFiles/create.js
@@ -1,23 +1,36 @@
 function create ()
-    {
-        carpet = this.add.image(0,0,'carpet').setOrigin(0,0);
+{
+    //Carpet background of level
+    this.carpet = this.add.image(0,0,'carpet').setOrigin(0,0);
+    this.carpet.setScale(.6);
 
-        carpet.setScale(.6);
+    //Wall group for all office walls that are not window boudries
+    var walls = this.physics.add.staticGroup();
 
-        wall = this.physics.add.image(200,200, 'wall');
+    walls.create(400,400, 'wall').setScale(.6).refreshBody();
+    walls.create(300,300, 'wall').setScale(.6).refreshBody();
 
-        circle = this.physics.add.image(100,100, 'circle');
-        circle.setCollideWorldBounds(true);
-        
-        //circle.setCollideWorldBounds(true);
 
-        // var circles = []
-        // for(var i = 0; i < 30; i++){
-        //     circles[i] = this.physics.add.image(i*30, 100+i*20, 'logo');
+    //Circle objects that below to this (Game)
+    this.circle = this.physics.add.image(100,100, 'circle');
+    //Keeps circle from leaving the windows of the Game
+    this.circle.setCollideWorldBounds(true);
 
-        //     circles[i].setVelocity(100, 200);
-        //     circles[i].setBounce(1, 1);
-        //     circles[i].setCollideWorldBounds(true);
+    //Sample of collision detection that will be used for enemies later.
+        //this.physics.add.collider(this.circle, walls, function func(){console.log("I'm sad")}, null, this);
 
-        // }
-    }
+    //Establishes collision between all objects in the walls staticGroup and this.circle
+    //NOTE: Will only work if the object is moved via velocity, and NOT xy cordinates. Learnt this the long and painful way.
+    this.physics.add.collider(walls, this.circle);
+
+
+    // var circles = []
+    // for(var i = 0; i < 100; i++){
+    //     circles[i] = this.physics.add.image(i*30, 100+i*20, '');
+
+    //     circles[i].setVelocity(100, 200);
+    //     circles[i].setBounce(1, 1);
+    //     circles[i].setCollideWorldBounds(true);
+    //     this.physics.add.collider(circles[i], this.circle);
+    // }
+}
