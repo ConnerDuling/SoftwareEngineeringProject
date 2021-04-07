3ef6c8ee25849f4bec7e118c308a76fa4778a5c9
Conner Duling <conner.m.duling@gmail.com>
2021-04-07 12:55:06 -0400

    Implemented Bug enemy type and random "skittering"

--- static/javascriptFiles/enemy.js
+++ static/javascriptFiles/enemy.js
@@ -2,9 +2,23 @@ function Enemy(game, xPosition, yPosition, bounceBool, setCollideBool, pictureNa
 
     this.picture = game.physics.add.image(xPosition, yPosition, pictureName);
 
+    //This enemy should bounce off the walls to move around
     if (bounceBool){
         this.picture.setBounce(1,1);
         this.picture.setVelocity(150,150);
+    }
+    //This enemy should "skitter" around and change direction after an interval.
+    else{
+
+        var scitterConfig = {
+            loop: true,
+            delay: 300,
+            callback: skitter,
+            args: [this.picture]
+        }
+
+        game.time.addEvent(scitterConfig);
+
     }
     
     //Sets collission with outer walls.
@@ -16,6 +30,25 @@ function Enemy(game, xPosition, yPosition, bounceBool, setCollideBool, pictureNa
     game.physics.add.collider(game.walls, this.picture);
     }
 };
+
+function skitter(picture){
+    var speed = 250
+    //console.log("Skittering");
+    picture.setVelocityX(speed * pickDirection());
+    picture.setVelocityY(speed * pickDirection());
+
+}
+
+function pickDirection(){
+    switch(Math.floor(Math.random() * 3)){
+        case 0:
+            return 1
+        case 1:
+            return 0
+        case 2:
+            return -1
+    }
+}
 
 //Sample of collision detection that will be used for enemies later.
 //this.physics.add.collider(this.circle, walls, function func(){console.log("I'm sad")}, null, this);
@@ -32,5 +65,5 @@ function FeatureCreep(game, xPosition, yPosition){
 
 function Bug(game, xPosition, yPosition){
 
-    Enemy.call(this, game, xPosition, yPosition, false, false, 'bug');
+    Enemy.call(this, game, xPosition, yPosition, false, true, 'bug');
 };
