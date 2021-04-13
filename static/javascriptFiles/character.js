1af28d4fc265e0a4a9a76183c22a2b8297bb4c66
Conner Duling <conner.m.duling@gmail.com>
2021-04-12 20:42:08 -0400

    Cleaned up Character class

--- static/javascriptFiles/character.js
+++ static/javascriptFiles/character.js
@@ -10,15 +10,10 @@ function Character(game, pictureName){
 
     //Overlap with workstation
     game.physics.add.overlap(game.workzones, this.picture, function workzoneOverlap(){
-        //console.log("You are touching the workstation");
         
         var eKey = game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
         if(Phaser.Input.Keyboard.JustDown(eKey)){
                 game.taskPts += 1;
         }
-
-        // game.workzones[0].setInteractive().on('pointerup', function pointGain(){
-        //     //if(this.picture.getBounds() == game.workzone[0].getBounds())
-        // });
     });
 }
