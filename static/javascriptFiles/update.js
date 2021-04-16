6fc4a2d68e1b3c836a8a2173d4fee923540f4652
ConnerDuling <conner.m.duling@gmail.com>
2021-04-16 13:36:19 -0400

    Update cycles through Character array now

--- static/javascriptFiles/update.js
+++ static/javascriptFiles/update.js
@@ -7,31 +7,38 @@ function update(){
 
 function characterMovement(gameObject){
     
-    var cursorKeys = gameObject.input.keyboard.createCursorKeys();
 
-    var speed = 200;
+    for(var i = 0; i < gameObject.characters.length; i++){
+
+        //Grab speed of character, in case their speed is modified.
+        var appliedSpeed = gameObject.characters[i].speed;
+
+        var cursorKeys = gameObject.input.keyboard.createCursorKeys();
+
+        //Calculates the real speed if moving diagonally
+        //and updates speed to the derived number for that frame.
+        if((cursorKeys.right.isDown || cursorKeys.left.isDown) &&
+        (cursorKeys.down.isDown || cursorKeys.up.isDown)){
+            appliedSpeed = Math.sqrt((appliedSpeed * appliedSpeed) / 2.0)
+        }
+        //Checks if user input of direction
+        //keys are pressed for that frame
+        if(cursorKeys.right.isDown){
+        gameObject.characters[i].picture.setVelocityX(appliedSpeed)
+        }else if(cursorKeys.left.isDown){
+            gameObject.characters[i].picture.setVelocityX((-appliedSpeed))
+        }else{
+            gameObject.characters[i].picture.setVelocityX(0);
+        }
+
+        if(cursorKeys.up.isDown){
+        gameObject.characters[i].picture.setVelocityY((-appliedSpeed))
+        }else if(cursorKeys.down.isDown){
+            gameObject.characters[i].picture.setVelocityY(appliedSpeed)
+        }else{
+            gameObject.characters[i].picture.setVelocityY(0);
+        }
+    }
+
     
-    //Calculates the real speed if moving diagonally
-    //and updates speed to the derived number for that frame.
-    if((cursorKeys.right.isDown || cursorKeys.left.isDown) &&
-    (cursorKeys.down.isDown || cursorKeys.up.isDown)){
-        speed = Math.sqrt((speed * speed) / 2.0)
-    }
-    //Checks if user input of direction
-    //keys are pressed for that frame
-    if(cursorKeys.right.isDown){
-       gameObject.circle.picture.setVelocityX(speed)
-    }else if(cursorKeys.left.isDown){
-        gameObject.circle.picture.setVelocityX((-speed))
-    }else{
-        gameObject.circle.picture.setVelocityX(0);
-    }
-
-    if(cursorKeys.up.isDown){
-       gameObject.circle.picture.setVelocityY((-speed))
-    }else if(cursorKeys.down.isDown){
-        gameObject.circle.picture.setVelocityY(speed)
-     }else{
-        gameObject.circle.picture.setVelocityY(0);
-    }
 }
