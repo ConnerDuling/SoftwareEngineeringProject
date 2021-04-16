22b2e43ff2a1001650b05e23b9052cb5e8d11d5d
ConnerDuling <conner.m.duling@gmail.com>
2021-04-16 15:43:54 -0400

    Changed Enemy to proper class

--- static/javascriptFiles/enemy.js
+++ static/javascriptFiles/enemy.js
@@ -1,60 +1,71 @@
-function Enemy(game, xPosition, yPosition, bounceBool, setCollideBool, pictureName){
+class Enemy extends Phaser.Physics.Arcade.Image {
+    constructor(game, xPosition, yPosition, bounceBool, setCollideBool, pictureName){
 
-    this.picture = game.physics.add.image(xPosition, yPosition, pictureName);
-    this.picture.setScale(.125)
-    this.picture.health = 3;
+        super(game, xPosition, yPosition, pictureName)
+        
+        
 
-    //This enemy should bounce off the walls to move around
-    if (bounceBool){
-        this.picture.setBounce(1,1);
-        this.picture.setVelocity(150,150);
-    }
-    //This enemy should "skitter" around and change direction after an interval.
-    else{
+        game.add.existing(this);
+        game.physics.add.existing(this);
 
-        var scitterConfig = {
-            loop: true,
-            delay: 300,
-            callback: skitter,
-            args: [this.picture]
+        //this = game.physics.add.image(xPosition, yPosition, pictureName);
+        this.setScale(.125);
+        this.health = 3;
+
+        console.log(this);
+
+        //This enemy should bounce off the walls to move around
+        if (bounceBool){
+            this.setBounce(1,1);
+            this.setVelocity(150,150);
+        }
+        //This enemy should "skitter" around and change direction after an interval.
+        else{
+
+            var scitterConfig = {
+                loop: true,
+                delay: 300,
+                callback: skitter,
+                args: [this]
+            }
+
+            game.time.addEvent(scitterConfig);
+
+        }
+        
+        //Sets collission with outer walls.
+        //ALL Enemies should have this trait, or else they will leave the scene
+        this.setCollideWorldBounds(true);
+
+        //Sets collission with inner walls
+        if(setCollideBool){
+            game.physics.add.collider(game.walls, this);
         }
 
-        game.time.addEvent(scitterConfig);
+        for(var i = 0; i < game.characters.length; i++){
+            game.physics.add.overlap(this, game.characters[i].picture, function takeDamage(enemy){
+                // console.log(enemy.displayList)
+                var eKey = game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
+                if(Phaser.Input.Keyboard.JustDown(eKey)){
+                        //Add check of Character type for extra damage from specialists
+                        //if() Enemy Weakness == Player Role
+                        //      Deal 2 damage
+                        //else
+                        //      We just deal one damage
 
+                        enemy.health -= 1;
+                        console.log(enemy.health);
+
+                        //If the enemy has been reduced to zero or less, destroy it
+                        if(enemy.health <= 0){
+                            enemy.destroy();
+                        }
+                        
+                }
+            });
+        }
     }
-    
-    //Sets collission with outer walls.
-    //ALL Enemies should have this trait, or else they will leave the scene
-    this.picture.setCollideWorldBounds(true);
-
-    //Sets collission with inner walls
-    if(setCollideBool){
-    game.physics.add.collider(game.walls, this.picture);
-    }
-
-    for(var i = 0; i < game.characters.length; i++){
-        game.physics.add.overlap(this.picture, game.characters[i].picture, function takeDamage(enemy){
-            // console.log(enemy.displayList)
-            var eKey = game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
-            if(Phaser.Input.Keyboard.JustDown(eKey)){
-                    //Add check of Character type for extra damage from specialists
-                    //if() Enemy Weakness == Player Role
-                    //      Deal 2 damage
-                    //else
-                    //      We just deal one damage
-
-                    enemy.health -= 1;
-                    console.log(enemy.health);
-
-                    //If the enemy has been reduced to zero or less, destroy it
-                    if(enemy.health <= 0){
-                        enemy.destroy();
-                    }
-                    
-            }
-        });
-    }
-};
+}
 
 function skitter(picture){
     var skitterSpeed = 250
@@ -82,17 +93,24 @@ function pickDirection(){
 //Sample of collision detection that will be used for enemies later.
 //this.physics.add.collider(this.circle, walls, function func(){console.log("I'm sad")}, null, this);
 
-function Monkey(game, xPosition, yPosition){
+class Monkey extends Enemy{
 
-    Enemy.call(this, game, xPosition, yPosition, true, true, 'monkey_enemy');
+    constructor(game, xPosition, yPosition){
+        super(game, xPosition, yPosition, true, true, 'monkey_enemy');
+    }
 };
 
-function FeatureCreep(game, xPosition, yPosition){
 
-    Enemy.call(this, game, xPosition, yPosition, true, false, 'feature_creep_enemy');
+class FeatureCreep extends Enemy{
+
+    constructor(game, xPosition, yPosition){
+        super(game, xPosition, yPosition, true, false, 'feature_creep_enemy');
+    }
 };
 
-function Bug(game, xPosition, yPosition){
+class Bug extends Enemy{
+    constructor(game, xPosition, yPosition){
 
-    Enemy.call(this, game, xPosition, yPosition, false, true, 'bug_enemy');
+    super(game, xPosition, yPosition, false, true, 'bug_enemy');
+    }
 };
