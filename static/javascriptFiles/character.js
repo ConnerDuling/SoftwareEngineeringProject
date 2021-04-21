3f11b5df537d81252828ea57af65c3c05b87ed30
ConnerDuling <conner.m.duling@gmail.com>
2021-04-21 13:07:01 -0400

    Fixed Tasks not being created properly.

--- static/javascriptFiles/character.js
+++ static/javascriptFiles/character.js
@@ -2,11 +2,11 @@ class Character extends Phaser.Physics.Arcade.Image {
     constructor(game, xPosition, yPosition, pictureName){
         super(game, xPosition, yPosition, pictureName);
         this.genTasks = [];
-        this.genTasks.push(new task("Make a Copy of A Copy.", 100));
-        this.genTasks.push(new task("Empty Trash.", 100));
-        this.genTasks.push(new task("Organize File Cabnet.", 100));
-        this.genTasks.push(new task("Water Fern.", 100));
-        this.genTasks.push(new task("Refill ink in the printer.", 100));
+        this.genTasks.push(new Task("Make a Copy of A Copy.", 100));
+        this.genTasks.push(new Task("Empty Trash.", 100));
+        this.genTasks.push(new Task("Organize File Cabnet.", 100));
+        this.genTasks.push(new Task("Water Fern.", 100));
+        this.genTasks.push(new Task("Refill ink in the printer.", 100));
         this.invuln = 0;
         this.fearFlag = 0;
         this.speed = 200;
@@ -43,11 +43,11 @@ class SoftwareProgrammer extends Character{
     constructor(game, xPosition, yPosition){
         super(game, xPosition, yPosition, 'red_circle');
         this.sProgTasks = [];
-        this.sProgTasks.push(new task("Implement feature.", 200));
-        this.sProgTasks.push(new task("Create an Algorithm.", 200));
-        this.sProgTasks.push(new task("Print your revisions of the code.", 200));
-        this.sProgTasks.push(new task("Create a class diagram.", 200));
-        this.sProgTasks.push(new task("Get permission from IT.", 200));
+        this.sProgTasks.push(new Task("Implement feature.", 200));
+        this.sProgTasks.push(new Task("Create an Algorithm.", 200));
+        this.sProgTasks.push(new Task("Print your revisions of the code.", 200));
+        this.sProgTasks.push(new Task("Create a class diagram.", 200));
+        this.sProgTasks.push(new Task("Get permission from IT.", 200));
     }
 }
 
@@ -55,11 +55,11 @@ class SoftwareDeveloper extends Character{
     constructor(game, xPosition, yPosition){
         super(game, xPosition, yPosition, 'green_circle');
         this.sDevTasks = [];
-        this.sDevTasks.push(new task("Research new software designs.", 200));
-        this.sDevTasks.push(new task("File your documentation.", 200));
-        this.sDevTasks.push(new task("Provide maintenence.", 200));
-        this.sDevTasks.push(new task("Make a sequence diagram.", 200));
-        this.sDevTasks.push(new task("Make some calls for research purposes.", 200));
+        this.sDevTasks.push(new Task("Research new software designs.", 200));
+        this.sDevTasks.push(new Task("File your documentation.", 200));
+        this.sDevTasks.push(new Task("Provide maintenence.", 200));
+        this.sDevTasks.push(new Task("Make a sequence diagram.", 200));
+        this.sDevTasks.push(new Task("Make some calls for research purposes.", 200));
     }
 }
 
@@ -67,11 +67,11 @@ class QualityTester extends Character{
     constructor(game, xPosition, yPosition){
         super(game, xPosition, yPosition, 'yellow_circle');
         this.qaTasks = [];
-        this.qaTasks.push(new task("Test the newest build.", 200));
-        this.qaTasks.push(new task("Write down any bugs encountered.", 200));
-        this.qaTasks.push(new task("File bug reports.", 200));
-        this.qaTasks.push(new task("Print out screenshot.", 200));
-        this.qaTasks.push(new task("Find out the reason why this bug exists.", 200));
+        this.qaTasks.push(new Task("Test the newest build.", 200));
+        this.qaTasks.push(new Task("Write down any bugs encountered.", 200));
+        this.qaTasks.push(new Task("File bug reports.", 200));
+        this.qaTasks.push(new Task("Print out screenshot.", 200));
+        this.qaTasks.push(new Task("Find out the reason why this bug exists.", 200));
     }
 }
 
@@ -79,10 +79,10 @@ class RequirementsEngineer extends Character{
     constructor(game, xPosition, yPosition){
         super(game, xPosition, yPosition, 'blue_circle');
         this.rEngTasks = [];
-        this.rEngTasks.push(new task("Make meeting with stakeholder.", 200));
-        this.rEngTasks.push(new task("Brainstorm ideas.", 200));
-        this.rEngTasks.push(new task("Make a diagram", 200));
-        this.rEngTasks.push(new task("Assemble a powerpoint.", 200));
-        this.rEngTasks.push(new task("Print out interview notes.", 200));
+        this.rEngTasks.push(new Task("Make meeting with stakeholder.", 200));
+        this.rEngTasks.push(new Task("Brainstorm ideas.", 200));
+        this.rEngTasks.push(new Task("Make a diagram", 200));
+        this.rEngTasks.push(new Task("Assemble a powerpoint.", 200));
+        this.rEngTasks.push(new Task("Print out interview notes.", 200));
     }
 }
