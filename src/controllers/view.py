b7853b0b11a80ad253d816890705e27114745294
openthething <shivansh181@gmail.com>
2021-04-22 19:21:29 -0400

    update

--- src/controllers/view.py
+++ src/controllers/view.py
@@ -1,11 +1,8 @@
 from flask import render_template, request, url_for, redirect
-from src import app
+from src import app, socketio
+from flask_socketio import join_room, leave_room
 
 @app.route('/')
-def index():
-    return render_template('game.html')
-
-@app.route('/landing_page')
 def landing_page():
     return render_template('index.html')
 
@@ -19,6 +16,13 @@ def chat():
     room = request.args.get('room')
 
     if username and room:
-        return render_template('chat.html', username=username, room=room)
+        # return render_template('game.html', username=username, room=room)
+        return render_template('game.html')
     else:
         return redirect(url_for('/room'))
+
+@socketio.on('join_room')
+def handle_join_room_event(data):
+    app.logger.info("{} has joined the room {}".format(data['username'], data['room']))
+    join_room(data['room'])
+    socketio.emit( 'join_room_announcement', data)
