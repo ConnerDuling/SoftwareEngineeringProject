432787180436dae8407606801efac4569a85f30b
ConnerDuling <conner.m.duling@gmail.com>
2021-04-26 23:00:07 -0400

    Clarified what is done on enemy destruction

--- src/static/javascriptFiles/enemy.js
+++ src/static/javascriptFiles/enemy.js
@@ -43,7 +43,7 @@ class Enemy extends Phaser.Physics.Arcade.Image {
                     //If the enemy has been reduced to zero or less, destroy it
                     console.log(enemy.health)
                     if(enemy.health <= 0){
-                        console.log(enemy)
+                        //Update appropriate count upon enemy being destroyed
                         if(enemy instanceof Bug){game.bugCount--}
                         else if(enemy instanceof SpaghettiCode){game.spaghettiCodeCount--}
                         else if(enemy instanceof Monkey){game.monkeyCount--}
