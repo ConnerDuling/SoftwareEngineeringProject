e4551281613339840e177ddf590e8f89742048c9
Jacob Drew <Drewje01@pfw.edu>
2021-04-27 19:03:03 -0400

    level 3 is designed and finished

--- src/static/javascriptFiles/level3.js
+++ src/static/javascriptFiles/level3.js
@@ -1,7 +1,7 @@
 class Level3 extends Phaser.Scene {
     constructor(){
         super('PlayLevel3')
-    }
+    }s
     preload ()
     {
         preloadGameImages(this)
@@ -12,14 +12,19 @@ class Level3 extends Phaser.Scene {
 
         //Wall group for all office walls that are not window boudries
         this.walls = this.physics.add.staticGroup();
+        this.walls.add(makeWall(this, this.cameras.main.width/4,this.cameras.main.height/3,15,500, 0xffffff))
+        this.walls.add(makeWall(this, (this.cameras.main.width/3)+150,(this.cameras.main.height/4)+300,450,15, 0xffffff))
+        this.walls.add(makeWall(this, (this.cameras.main.width/3)+370, this.cameras.main.height/2,15,300, 0xffffff))
+        this.walls.add(makeWall(this, (this.cameras.main.width/3)+225, (this.cameras.main.height/2)-150,300, 15, 0xffffff))
+        
 
         //Workzone objects
         this.workzones = [4]
         var workStationOffset = 108;
-        this.workzones[0] = new BlueWorkStation(this,0,0);
-        this.workzones[1] = new RedWorkStation(this,this.cameras.main.width-workStationOffset,0);
-        this.workzones[2] = new GreenWorkStation(this, 0,this.cameras.main.height-workStationOffset);
-        this.workzones[3] = new YellowWorkStation(this, this.cameras.main.width-workStationOffset,this.cameras.main.height-workStationOffset)
+        this.workzones[0] = new BlueWorkStation(this,522,158);
+        this.workzones[1] = new RedWorkStation(this,(this.cameras.main.width-workStationOffset-485),335);
+        this.workzones[2] = new GreenWorkStation(this, 522,(this.cameras.main.height-workStationOffset)-157);
+        this.workzones[3] = new YellowWorkStation(this, (this.cameras.main.width-workStationOffset)-485,(this.cameras.main.height-workStationOffset)-335)
         
         //Character objects that belong to this (Scene)
         this.characters = []
