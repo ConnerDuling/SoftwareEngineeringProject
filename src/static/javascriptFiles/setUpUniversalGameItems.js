9adfa369408340e0f27deb62ccbe201c1aaf88d2
ConnerDuling <conner.m.duling@gmail.com>
2021-04-26 23:00:24 -0400

    Clarified image preloading

--- src/static/javascriptFiles/setUpUniversalGameItems.js
+++ src/static/javascriptFiles/setUpUniversalGameItems.js
@@ -1,4 +1,4 @@
-function loadGameImages(game){
+function preloadGameImages(game){
     game.load.text('generic', 'static/assets/text/genericTasks.txt');
     game.load.image('carpet', 'static/assets/images/CarpetBackground.png');
     
@@ -6,12 +6,11 @@ function loadGameImages(game){
     game.load.image('red_circle', 'static/assets/images/CircleRedTransperantBackground.png');
     game.load.image('yellow_circle', 'static/assets/images/CircleYellowTransperantBackground.png');
     game.load.image('blue_circle', 'static/assets/images/CircleBlueTransperantBackground.png');
+
     game.load.image('soft_Dev', 'static/assets/images/Player3.png');
     game.load.image('soft_Prog', 'static/assets/images/Player2.png');
     game.load.image('qa_Test', 'static/assets/images/Player4.png');
     game.load.image('req_Eng', 'static/assets/images/Player1.png');
-
-    game.load.image('wall', 'static/assets/images/protoWall.png')
 
     game.load.image('bug_enemy', 'static/assets/images/EnemyBug.png')
     game.load.image('feature_creep_enemy', 'static/assets/images/EnemyFeatureCreep.png')
@@ -23,6 +22,8 @@ function loadGameImages(game){
     game.load.image('green_workzone', 'static/assets/images/SquareGreenTransperantBackground.png')
     game.load.image('yellow_workzone', 'static/assets/images/SquareYellowTransperantBackground.png')
 }
+
+
 
 function addNewEnemyToGame(enemies, game){
 
@@ -60,6 +61,33 @@ function addNewEnemyToGame(enemies, game){
     }
 }
 
+function setUpEnemySpawnSystem(game){
+
+    game.enemies = [];
+
+    //Limiters on enemy count for each enemy type
+    game.monkeyCount = 0;
+    game.bugCount = 0;
+    game.featureCreepCount = 0;
+    game.spaghettiCodeCount = 0;
+
+    //Set up enemy spawner for the Game
+    var spawnDelaySeconds = 5
+    var spawnConfig = {
+        loop: true,
+        delay: (1000 * spawnDelaySeconds),
+        callback: addNewEnemyToGame,
+        args: [game.enemies, game]
+    }
+    game.time.addEvent(spawnConfig);
+}
+
 function makeWall(game, xCord, yCord, height, width){
     return game.add.rectangle(xCord,yCord,height,width, 0xffffff)
+}
+
+function makeCarpet(game){
+    //Carpet background of levels
+    game.carpet = game.add.image(0,0,'carpet').setOrigin(0,0);
+    game.carpet.setScale(.6);
 }
